b1713bb82edc76d37ecba5972a58cef29c91b39e
42trustokerezi <trustokerezi42@gmail.com>
2023-12-28 10:37:20 +0100

    search ui created

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex-col bg-[#0D1016] items-center justify-between px-8 pt-36">
       <h1 className='text-white font-normal text-4xl md:text-6xl'>Find the best getaways for you.</h1>
-      <div className='w-[80%] mx-auto'>
+      <div className='w-[100%] mx-auto mt-12'>
         <Search/>
       </div>
     </main>
--- components/search/search.jsx
+++ components/search/search.jsx
@@ -1,9 +1,46 @@
-import style from './search.module.css'
+import style from "./search.module.css";
+import { MdSearch, MdCalendarMonth, MdPerson } from "react-icons/md";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="grid grid-cols-4 w-full md:w-[60%] h-[120px] border-2 rounded-lg border-[#212A39] bg-[#16202F]">
+      <div className="col-span-3 mx-3">
+        <div className="w-[100%] border-b py-3 border-[#425472] flex">
+          <MdSearch style={{ color: "#425472", width: "2em", height: "2em" }} />
+          <input
+          type="text"
+            className="w-full ml-2 bg-transparent placeholder-[#425472] font-light"
+            placeholder="find your desired stay.."
+          />
+        </div>
+        <div className="flex justify-between py-3">
+        <div className="flex pr-4 border-r border-[#425472]">
+          <MdCalendarMonth
+            style={{ color: "#425472", width: "1em", height: "1.5em" }}
+          />
+          <p className="font-light text-[#425472]">from</p>
+        </div>
+        <div className="flex pr-4 border-r border-[#425472]">
+        <MdCalendarMonth
+            style={{ color: "#425472", width: "1em", height: "1.5em" }}
+          />
+          <p className="font-light text-[#425472]">to</p>
+        </div>
+        <div className="flex">
+        <MdPerson
+            style={{ color: "#425472", width: "1em", height: "1.5em" }}
+          />
+          <p className="font-light text-[#425472]">count</p>
+        </div>
+        </div>
+        
+      </div>
 
-export default Search
+      <button className="w-full bg-gray-950 px-3">
+        <MdSearch style={{ color: "#425472", width: "3em", height: "3em" }} />
+      </button>
+    </div>
+  );
+};
+
+export default Search;
